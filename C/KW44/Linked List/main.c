194a8e04b3ce23ca48089657d2dbfc71057f37cc
Loris Di Leo <loris.dileo@noseryoung.com>
2022-11-02 15:51:09 +0100

    Codezeilen hinzugefügt + file erstellt

---
+++ C/KW44/Linked List/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data
+    struct Node *next
+}TNode;
+
+int main() {
+
+TNode *list = NULL;
+
+list = (TNode*)malloc(sizeof(TNode));
+
+list -> next = NULL;
+
+list -> data = (TNode*)malloc(sizeof(TNode));
+
+list -> data *c = 'A';
+
+list -> next = (TNode *)malloc(sizeof(TNode));
+
+list -> next -> next = NULL;
+
+list -> next -> data = (TNode *)malloc(sizeof(TNode));
+
+list -> next -> data -> c = 'C';
+
+
+
+
+}
