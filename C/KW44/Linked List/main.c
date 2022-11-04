9ed9425b5422a24bfee328590355b005d21466b6
Loris Di Leo <loris.dileo@noseryoung.com>
2022-11-04 15:38:27 +0100

    Codezeilen hinzugefügt (Aufgabe 9 + 10 von Linked List)

--- C/KW44/Linked List/main.c
+++ C/KW44/Linked List/main.c
@@ -17,9 +17,52 @@ void anyList(TNode *head){
     }    
 }
 
+void printList(TNode *node)
+{
+    while (node != NULL) {
+        printf(" %d ", node->data);
+        node = node->next;
+    }
+}
+
+void deleteNode(TNode *head, unsigned int n){
+
+    TNode *nodeToDelete = head;
+
+    for (int i = 0; i < n - 1; i++) 
+    {
+        nodeToDelete = nodeToDelete -> next;
+    }
+
+    TNode *addressCache = nodeToDelete -> next -> next;
+
+    free(nodeToDelete -> next);
+
+    nodeToDelete -> next = addressCache;
+
+}
+
 TNode *createNode(){
     TNode *newNode = (TNode *) malloc(sizeof(TNode));
     return newNode;
+}
+
+void initNode(TNode *head, unsigned int n, int data){
+
+    TNode *nodeToInit = head;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        nodeToInit = nodeToInit -> next;
+    }
+
+    TNode *addressInit = nodeToInit -> next;
+
+    nodeToInit -> next  = (TNode *) malloc(sizeof(TNode));
+
+    nodeToInit -> next -> data = data;
+    nodeToInit -> next -> next = addressInit;
+    
 }
 
 int main() {
@@ -35,18 +78,20 @@ int main() {
     list3 -> next = NULL;
 
     head -> data = 0;
-    list1 -> data = 3;
-    list2 -> data = 8;
-    list3 -> data = 6;
+    list1 -> data = 1;
+    list2 -> data = 2;
+    list3 -> data = 3;
 
-    anyList(head);
+    deleteNode(head, 2);
 
+    TNode *list4 = createNode();
+    list3 -> next = list4;
+    list4 -> next = NULL;
+    list4 -> data = 4;
+
+    initNode(head, 4, 11);
+
+    printList(head);
+    
     return 0;
 }
-
-/*node delete_right(node list){
-	node tmp   = list->next;
-	list->next = list->next->next;
-	free(tmp);
-	return list;
-}*/
