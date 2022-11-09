7f88000beff17f3a661790b2c01de80dfbdddd01
Loris Di Leo <loris.dileo@noseryoung.com>
2022-11-09 11:55:27 +0100

    Codezeilen hinzugefügt(neue Person erstellt)

--- Java/KW45/PersonManager/src/Main.java
+++ Java/KW45/PersonManager/src/Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
        person Sairam = new person("Sairam");
+       person Jessica = new person("Jessica");
 
        Sairam.setFavoriteColor("blau");
+       Jessica.setFavoriteColor("grün");
 
         System.out.println("***********************");
         Sairam.presentYourself();
+        System.out.println("***********************");
+        Jessica.presentYourself();
         System.out.println("***********************");
     }
 }
