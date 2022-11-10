8b195a957ea0e6ea3f95dc5508ca445b2d76c07b
Loris Di Leo <loris.dileo@noseryoung.com>
2022-11-10 11:23:31 +0100

    Aufgabe gelöst, Codezeilen hinzugfügt, ordner erstllt

---
+++ Arduino/KW45/0001-setup-test/0001-setup-test.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(115200);// initial serial port speed (baud)
+  Serial.println("SETUP DONE");
+  Serial.println("----------");
+}
+void loop() {
+  Serial.println("THE END IS NEVER");
+  delay(500);
+}
