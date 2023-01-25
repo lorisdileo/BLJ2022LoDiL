b9e98cb5518f3d6a7d1329399b473fc89dceb00c
Loris Di Leo <112620739+lorisdileo@users.noreply.github.com>
2023-01-25 12:13:23 +0100

    Delete Java/KW45 directory

--- Java/KW45/ThreeTwoOne/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Three");
-        System.out.println("Two");
-        System.out.println("One");
-        System.out.println("Takeoff!!");
-    }
-}
