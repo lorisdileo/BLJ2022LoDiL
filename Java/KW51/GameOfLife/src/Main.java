767017083d6e04839fa86ad06f7ee13e5ae188e9
Loris Di Leo <112620739+lorisdileo@users.noreply.github.com>
2023-01-25 12:14:43 +0100

    Delete Java/KW51 directory

--- Java/KW51/GameOfLife/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Simulation sim = new Simulation(100, true, Preset.GLIDERS_20X20);
-        SimulationPanel simPanel = new SimulationPanel(sim, 40, 0.9);
-        simPanel.getSimulation().start();
-    }
-}
