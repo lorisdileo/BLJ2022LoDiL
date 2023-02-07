ae0bdca2c6b801456019b8f54bc48a5cd85e7aa7
Loris Di Leo <loris.dileo@noseryoung.com>
2023-02-07 14:56:07 +0100

    Aufgabe gelöst, beide Funktionen gemacht

--- Java/2022/KW49/Konfektionsgroesse/src/Formel.java
+++ Java/2022/KW49/Konfektionsgroesse/src/Formel.java
@@ -1,8 +1,36 @@
+import java.util.IllegalFormatCodePointException;
+import java.util.Scanner;
+
 public class Formel {
+    static Scanner scan = new Scanner(System.in);
 
-    private
+    public static void mannBerechnung() {
+        System.out.print("Geben Sie Ihre Grösse an: \t");
+        Integer hoehe = scan.nextInt();
 
-    private void mannBerechnung {
-        System.out.println("gib dein ");
+        System.out.print("Geben Sie Ihr Brustumfang an: \t");
+        Integer brustumfang = scan.nextInt();
+
+        System.out.println(hoehe + "cm gross, mit " + brustumfang + " cm Brustumfang: " + brustumfang / 2);
+    }
+
+    public static void frauBerechnung() {
+        System.out.print("Geben Sie Ihre Grösse an: \t");
+        Integer hoehe = scan.nextInt();
+
+        System.out.print("Geben Sie Ihr Brustumfang an: \t");
+        Integer brustumfang = scan.nextInt();
+
+        if (hoehe > 170){
+                System.out.println(hoehe + "cm gross, mit " + brustumfang + " cm Brustumfang: " + ((brustumfang / 2 - 6 )* 2));
+        }
+
+        if (hoehe < 164){
+                System.out.println(hoehe + "cm gross, mit " + brustumfang + " cm Brustumfang: " + ((brustumfang / 2 - 6) / 2));
+        }
+
+        if (hoehe > 164 && hoehe < 170){
+            System.out.println(hoehe + "cm gross, mit " + brustumfang + " cm Brustumfang: " + (brustumfang / 2 - 6));
+        }
     }
 }
--- Java/2022/KW49/Konfektionsgroesse/src/Main.java
+++ Java/2022/KW49/Konfektionsgroesse/src/Main.java
@@ -1,4 +1,5 @@
-import  java.util.Scanner;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -11,21 +12,12 @@ public class Main {
 
         int geschlecht = scan.nextInt();
 
-        switch (geschlecht){
-            case -> 1{
-            } {
+        switch (geschlecht) {
+            case 1: Formel.mannBerechnung();
+            break;
 
-            }
-             case -> 2 {
-
-
-            }
-
-
-
-
-
+            case 2: Formel.frauBerechnung();
+            break;
         }
-
     }
 }
