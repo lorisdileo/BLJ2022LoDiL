9b621f5dabc3de2219e4b85457490bdd0fed1a09
Loris Di Leo <loris.dileo@noseryoung.com>
2023-03-10 11:37:30 +0100

    Aufgabe fertig gemacht

---
+++ Java/2023/KW10/Ascii-Image/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        function fc = new function();
+
+        fc.aci();
+    }
+}
